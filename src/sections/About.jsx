94f1cdf7e2c10b664d5b90478b3729f1677eb2a1
Varun Devgan <varun.devgan@outlook.com>
2025-08-09 17:37:28 +0530

    corrected file name

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import Card from "../components/Card";
-import { Globe } from "../components/globe";
+import { Globe } from "../components/Globe";
 import CopyEmailButton from "../components/CopyEmailButton";
 import { Frameworks } from "../components/FrameWorks";
 
